75d8707a71e3c3b662b9d73b5c2e6feee07797ad
ronitshinde <rony.shinde753@gmail.com>
2023-05-04 19:47:52 +0530

    [Ronit] Add : UC1-Code to study the types of constructors

---
+++ 3BOopsConceptPractiseProblem/3BOopsConceptPractiseProblem/ConstructBuilding.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3BOopsConceptPractiseProblem
+{
+    //Constructor name is same as class name
+    //It cannot create objects
+    public class ConstructBuilding
+    {
+        public string Input;
+        public int faxNumber;
+        //This is parameterless constructor
+        public ConstructBuilding()
+        {
+            Console.WriteLine("This is parameterless constructor");
+        }
+        //This is a parameterized constructor
+        public ConstructBuilding(string data, int pinCode)
+        {
+            Input = data;
+            Console.WriteLine($"This is a parameterised constructor, parameter :  {Input} , {pinCode}");
+            Console.WriteLine(Input);
+        }
+        public ConstructBuilding(string Name, int faxNumber, char grade, int numOne, int numTwo)
+        {
+            Input = Name;
+            this.faxNumber = faxNumber;
+            int multiplication = numOne * numTwo;
+            Console.WriteLine("Multiplication of two numbers : " + multiplication);
+            Console.WriteLine("He has received grade : " + grade);
+            Console.WriteLine("Bridgelabz with fax number : " + faxNumber);
+            Console.WriteLine(Input);
+        }
+    }
+}
---
+++ 3BOopsConceptPractiseProblem/3BOopsConceptPractiseProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3BOopsConceptPractiseProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //As object cannot be created of a constructor this is the way to call a constructor
+            new ConstructBuilding();
+            new ConstructBuilding("BridgeLabz",455213);
+            new ConstructBuilding("Velocity",746321,'A',51,64);
+            Console.ReadKey();
+        }
+    }
+}
